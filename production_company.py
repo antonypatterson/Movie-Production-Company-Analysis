55703fdf2a80d9be3690c3eaac89666864d68094
antonypatterson <antonypatterson@hotmail.com>
2023-02-26 18:50:53 +1100

    added csv to gitignore and expanded search queries to multiple pages

--- production_company.py
+++ production_company.py
@@ -1,13 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-class ProductionCompany:
+class ProductionCompanyIMBD:
     def __init__(self, name="", url="", id=""):
         self._name = name
         self._company_url = url
         self._company_id = id
 
-    def search_by_keyword(self, keyword):
+    def get_average_rating(self, export_csv=False):
+        #only calls "private" method if company id not already assigned and self._name is already assigned
+        if not self._company_id and self._name:
+            self.__search_by_keyword(self._name)
+
+        if not self._average_rating and self._company_id:
+
+            # Construct the URL for the company's IMDB page and headers            
+            base_url = "https://www.imdb.com/search/title/"          
+
+            headers = {
+                "Accept-Language": "en-US,en;q=0.8",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+            }
+
+            # create empty dataframe
+            df = pd.DataFrame(columns=["title", "rating"])
+
+            #set starting index and number of movies to retrieve
+            start = 1
+            num_movies_tag = soup.find('span', class_='desc').text
+            num_movies = num_movies_tag.split(' of ')[-1].split(' titles')[0].replace(',', '')
+            
+            while start <= num_movies:  
+                query_string = f"?companies={self._company_id}&start={start}"
+                response = requests.get(base_url + query_string, headers=headers)
+
+                # Use Beautiful Soup to parse the HTML content of the company's IMDB page
+                soup = BeautifulSoup(response.text, "html.parser")
+
+                 # find all the movie titles and ratings on the page
+                titles = soup.find_all('h3', class_='lister-item-header')
+                #ratings = soup.find_all('div', class_='inline-block ratings-imdb-rating')
+
+                # loop through title elements and extract title and rating
+                for title in titles:
+                    # extract title text
+                    title_text = title.find("a").text.strip()
+                    
+                    # extract rating value from data-value attribute
+                    rating_value = title.find("div", class_="inline-block ratings-imdb-rating")["data-value"]
+                    
+                    # add title and rating to dataframe
+                    df = df.append({"title": title_text, "rating": rating_value}, ignore_index=True)
+                
+                # increase start index by 50 for the next page
+                start += 50
+
+        if export_csv:
+            df.to_csv(f"{self._name}_titles_{1}_to_{start+49}.csv", index=False)
+
+        return self._average_rating
+
+    def __search_by_keyword(self, keyword):
         # Construct the search URL
         search_url = f"https://www.imdb.com/find?q={keyword}&s=co"
 
@@ -26,6 +80,9 @@ class ProductionCompany:
             self._company_id = company_url.split("/")[-2]
 
         return self
+    
+    
+        
 
     @property
     def name(self):
